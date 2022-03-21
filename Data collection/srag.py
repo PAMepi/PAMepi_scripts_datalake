64e7d3cff88fca48a4f08d2672c1f033e910dced
marconso <marconsodehsfilho@gmail.com>
2022-03-20 21:52:13 -0400

    patched

--- Data collection/srag.py
+++ Data collection/srag.py
@@ -15,26 +15,22 @@ output_srag = os.path.join(datalake, raw, 'data-sindrome_respiratoria_aguda_grav
 
 pathlib.Path(output_srag).mkdir(parents=True, exist_ok=True)
 
-# main url
 url = 'https://opendatasus.saude.gov.br/dataset/'
+endpoints = [
+    'srag-2019',
+    'srag-2020',
+    'srag-2021-e-2022',
+]
 
-srag_2020 = 'srag-2020-banco-de-dados-de-sindrome-respiratoria-aguda-grave-incluindo-dados-da-covid-19'
+for endpoint in endpoints:
+    r = requests.get(os.path.join(url, endpoint))
 
-srag_2021 = 'bd-srag-2021'
-
-# getting the page
-for srag in [srag_2020, srag_2021]:
-    r = requests.get(os.path.join(url, srag))
-
-    # parsing the page
     soup = BeautifulSoup(r.text, 'html.parser')
 
-    # pickup all elements <a> in page
     tag_a = soup.findAll('a', class_='heading')
 
     link = ''
     for tag in tag_a:
-        # cleaning spaces in blank and \n on element
         string = tag.text.lstrip('\n').rstrip('\n').lstrip(' ').rstrip(' ')[0:4]
         if string == 'SRAG':
             link = '/'.join(tag['href'].split('/')[2:])
@@ -44,7 +40,6 @@ for srag in [srag_2020, srag_2021]:
 
     soup = BeautifulSoup(r.text, 'html.parser')
 
-    # pickup all elements <a> in page
     tag_a = soup.findAll('a', class_='resource-url-analytics')
 
     for tag in tag_a:
@@ -52,7 +47,6 @@ for srag in [srag_2020, srag_2021]:
             file_csv = requests.get(tag['href'], stream=True)
             name = ''.join(tag.text.split('/')[-1:])
 
-            # opening the file and defining config for progress bar
             with open(os.path.join(output_srag, name), 'wb') as f, tqdm(
                 desc=tag.text,
                 total=int(file_csv.headers['Content-Length']),
@@ -60,8 +54,6 @@ for srag in [srag_2020, srag_2021]:
                 unit_scale=True,
                 unit_divisor=1024
             ) as bar:
-                # iterating chunk and writing on file opened
                 for content in file_csv.iter_content(chunk_size=1024):
                     size = f.write(content)
-                    # updating progress bar
                     bar.update(size)
