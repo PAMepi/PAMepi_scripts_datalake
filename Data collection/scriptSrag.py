da22d858e9263004cba01c2a1bd3bdef61da2755
marconso <marconsodehsfilho@gmail.com>
2022-04-15 08:09:29 -0400

    fix download

--- Data collection/scriptSrag.py
+++ Data collection/scriptSrag.py
@@ -13,6 +13,7 @@ from config import DATALAKE
 # set folders
 raw = 'raw_data_covid19_version-' + datetime.now().strftime('%Y-%m-%d')
 output_srag = os.path.join(DATALAKE, raw, 'data-sindrome_respiratoria_aguda_grave_incluindo_covid/')
+output_srag = 'datalake'
 
 pathlib.Path(output_srag).mkdir(parents=True, exist_ok=True)
 
@@ -30,31 +31,32 @@ for endpoint in endpoints:
 
     tag_a = soup.findAll('a', class_='heading')
 
-    link = ''
+    links = []
     for tag in tag_a:
         string = tag.text.lstrip('\n').rstrip('\n').lstrip(' ').rstrip(' ')[0:4]
         if string == 'SRAG':
-            link = '/'.join(tag['href'].split('/')[2:])
+            links.append('/'.join(tag['href'].split('/')[2:]))
 
 
-    r = requests.get(os.path.join(url, link))
+    for link in links:
+        r = requests.get(os.path.join(url, link))
 
-    soup = BeautifulSoup(r.text, 'html.parser')
+        soup = BeautifulSoup(r.text, 'html.parser')
 
-    tag_a = soup.findAll('a', class_='resource-url-analytics')
+        tag_a = soup.findAll('a', class_='resource-url-analytics')
 
-    for tag in tag_a:
-        if tag['href'].endswith('.csv') and tag['href'] == tag.text:
-            file_csv = requests.get(tag['href'], stream=True)
-            name = ''.join(tag.text.split('/')[-1:])
+        for tag in tag_a:
+            if tag['href'].endswith('.csv') and tag['href'] == tag.text:
+                file_csv = requests.get(tag['href'], stream=True)
+                name = ''.join(tag.text.split('/')[-1:])
 
-            with open(os.path.join(output_srag, name), 'wb') as f, tqdm(
-                desc=tag.text,
-                total=int(file_csv.headers['Content-Length']),
-                unit='iB',
-                unit_scale=True,
-                unit_divisor=1024
-            ) as bar:
-                for content in file_csv.iter_content(chunk_size=1024):
-                    size = f.write(content)
-                    bar.update(size)
+                with open(os.path.join(output_srag, name), 'wb') as f, tqdm(
+                    desc=tag.text,
+                    total=int(file_csv.headers['Content-Length']),
+                    unit='iB',
+                    unit_scale=True,
+                    unit_divisor=1024
+                ) as bar:
+                    for content in file_csv.iter_content(chunk_size=1024):
+                        size = f.write(content)
+                        bar.update(size)
